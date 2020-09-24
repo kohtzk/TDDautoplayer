cee75020eb05034b27a3622f67cc74bda99a0146
Kieran Kohtz <kohtzk@gmail.com>
2020-09-24 20:15:55 +0100

    added screen size modifiers

--- testing.py
+++ testing.py
@@ -11,12 +11,13 @@ failuredice = [1, 2, 4, 1]
 # failuredice = [1]
 # failuredice = [2, 1]
 optimal = 5.485
-screen = 1920
+screenx = 1920
+screeny = 0
 loop = True
 
 
 def checkpixelcolor(x, y, r, g, b):
-    pixel = pyautogui.pixel(screen + x, y)
+    pixel = pyautogui.pixel(screenx + x, screeny + y)
     if pixel[0] == r and pixel[1] == g and pixel[2] == b:
         return True
     else:
@@ -30,8 +31,8 @@ def detectkey():
 
 
 def isplayersturn():
-    pixel1 = pyautogui.pixel(screen + 157, 130)
-    pixel2 = pyautogui.pixel(screen + 291, 118)
+    pixel1 = pyautogui.pixel(screenx + 157, screeny + 130)
+    pixel2 = pyautogui.pixel(screenx + 291, screeny + 118)
     if pixel1[0] == pixel2[0] and pixel1[1] == pixel2[1] and pixel1[2] == pixel2[2] and pixel1[0] == 218 and pixel1[
         1] == 165 and pixel1[2] == 32:
         return True
@@ -69,21 +70,21 @@ def waittilplayerturn():
 
 
 def collect():
-    pyautogui.moveTo(screen + 150, 600)
-    pyautogui.dragTo(screen + 1700, 600, 1)
+    pyautogui.moveTo(screenx + 150, screeny + 600)
+    pyautogui.dragTo(screenx + 1700, screeny + 600, 1)
 
 
 def roll(times):
     for i in range(times):
         for j in range(4):
-            pyautogui.click(screen + 200 + dicespacing * j, 920)
+            pyautogui.click(screenx + 200 + dicespacing * j, screeny + 920)
         time.sleep(1)  # only needs 0.6 secs for dice to be clickable again
-        pixel = pyautogui.pixel(screen + 1461, 858)
+        pixel = pyautogui.pixel(screenx + 1461, screeny + 858)
         if pixel[0] != 255 or pixel[1] != 255 or pixel[2] != 255:
             # print("ROLLED A 1!")
             time.sleep(1.5)
             return
-    pyautogui.click(screen + 1461, 858)
+    pyautogui.click(screenx + 1461, screeny + 858)
     time.sleep(4)
     collect()
 
@@ -102,39 +103,39 @@ def rollround():
 
 def docrates():
     for i in range(7):
-        pyautogui.click(screen + 900, 300)
+        pyautogui.click(screenx + 900, screeny + 300)
         time.sleep(1)
     for i in range(10):
-        pyautogui.click(screen + chestpos[0] + i*chestspacing, chestpos[1])
+        pyautogui.click(screenx + chestpos[0] + i*chestspacing, screeny + chestpos[1])
     collect()
     collect()
     collect()
     collect()
-    pyautogui.click(screen + 900, 500)
+    pyautogui.click(screenx + 900, screeny + 500)
     time.sleep(2);
 
 
 def doendseq():
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(5)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
 
 
 def dostartseq():
-    pyautogui.click(screen + 1300, 500)
+    pyautogui.click(screenx + 1600, screeny + 950)
     time.sleep(1)
-    pyautogui.click(screen + 1600, 950)
+    pyautogui.click(screenx + 1600, screeny + 950)
 
 
 def restart():
